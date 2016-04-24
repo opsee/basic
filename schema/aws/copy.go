4b15765a3c4d07847fd7a6852a1ed6dcc5a90ff8
Mark Martin <assplecake@gmail.com>
2016-04-24 16:25:04 -0700

    copy timestamps to time

--- schema/aws/copy.go
+++ schema/aws/copy.go
@@ -45,6 +45,11 @@ func rcopy(dst, src reflect.Value, root bool) {
 				timestamp.Scan(*tt)
 				dst.Set(reflect.ValueOf(timestamp))
 			}
+		} else if tt, ok := src.Interface().(*opsee_types.Timestamp); ok {
+			if tt != nil && dst.CanSet() {
+				t, _ := tt.Value()
+				dst.Set(reflect.ValueOf(t.(time.Time)).Addr())
+			}
 		} else if dst.Kind() != reflect.Ptr {
 			if dst.CanSet() && !src.IsNil() {
 				dst.Set(reflect.Indirect(src))
