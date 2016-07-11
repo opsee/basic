f7b80fc3d76704745b2d11b94053ccf03d28ad74
Mark Martin <assplecake@gmail.com>
2016-07-11 11:10:40 -0700

    add vape endpoints to cats

--- service/cats.pb.go
+++ service/cats.pb.go
@@ -249,6 +249,14 @@ const _ = grpc.SupportPackageIsVersion3
 
 type CatsClient interface {
 	GetCheckCount(ctx context.Context, in *GetCheckCountRequest, opts ...grpc.CallOption) (*GetCheckCountResponse, error)
+	GetUser(ctx context.Context, in *GetUserRequest, opts ...grpc.CallOption) (*GetUserResponse, error)
+	UpdateUser(ctx context.Context, in *UpdateUserRequest, opts ...grpc.CallOption) (*UserTokenResponse, error)
+	ListUsers(ctx context.Context, in *ListUsersRequest, opts ...grpc.CallOption) (*ListUsersResponse, error)
+	InviteUser(ctx context.Context, in *InviteUserRequest, opts ...grpc.CallOption) (*InviteUserResponse, error)
+	DeleteUser(ctx context.Context, in *DeleteUserRequest, opts ...grpc.CallOption) (*DeleteUserResponse, error)
+	GetTeam(ctx context.Context, in *GetTeamRequest, opts ...grpc.CallOption) (*GetTeamResponse, error)
+	UpdateTeam(ctx context.Context, in *UpdateTeamRequest, opts ...grpc.CallOption) (*UpdateTeamResponse, error)
+	DeleteTeam(ctx context.Context, in *DeleteTeamRequest, opts ...grpc.CallOption) (*DeleteTeamResponse, error)
 }
 
 type catsClient struct {
@@ -268,10 +276,90 @@ func (c *catsClient) GetCheckCount(ctx context.Context, in *GetCheckCountRequest
 	return out, nil
 }
 
+func (c *catsClient) GetUser(ctx context.Context, in *GetUserRequest, opts ...grpc.CallOption) (*GetUserResponse, error) {
+	out := new(GetUserResponse)
+	err := grpc.Invoke(ctx, "/opsee.Cats/GetUser", in, out, c.cc, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *catsClient) UpdateUser(ctx context.Context, in *UpdateUserRequest, opts ...grpc.CallOption) (*UserTokenResponse, error) {
+	out := new(UserTokenResponse)
+	err := grpc.Invoke(ctx, "/opsee.Cats/UpdateUser", in, out, c.cc, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *catsClient) ListUsers(ctx context.Context, in *ListUsersRequest, opts ...grpc.CallOption) (*ListUsersResponse, error) {
+	out := new(ListUsersResponse)
+	err := grpc.Invoke(ctx, "/opsee.Cats/ListUsers", in, out, c.cc, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *catsClient) InviteUser(ctx context.Context, in *InviteUserRequest, opts ...grpc.CallOption) (*InviteUserResponse, error) {
+	out := new(InviteUserResponse)
+	err := grpc.Invoke(ctx, "/opsee.Cats/InviteUser", in, out, c.cc, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *catsClient) DeleteUser(ctx context.Context, in *DeleteUserRequest, opts ...grpc.CallOption) (*DeleteUserResponse, error) {
+	out := new(DeleteUserResponse)
+	err := grpc.Invoke(ctx, "/opsee.Cats/DeleteUser", in, out, c.cc, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *catsClient) GetTeam(ctx context.Context, in *GetTeamRequest, opts ...grpc.CallOption) (*GetTeamResponse, error) {
+	out := new(GetTeamResponse)
+	err := grpc.Invoke(ctx, "/opsee.Cats/GetTeam", in, out, c.cc, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *catsClient) UpdateTeam(ctx context.Context, in *UpdateTeamRequest, opts ...grpc.CallOption) (*UpdateTeamResponse, error) {
+	out := new(UpdateTeamResponse)
+	err := grpc.Invoke(ctx, "/opsee.Cats/UpdateTeam", in, out, c.cc, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *catsClient) DeleteTeam(ctx context.Context, in *DeleteTeamRequest, opts ...grpc.CallOption) (*DeleteTeamResponse, error) {
+	out := new(DeleteTeamResponse)
+	err := grpc.Invoke(ctx, "/opsee.Cats/DeleteTeam", in, out, c.cc, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // Server API for Cats service
 
 type CatsServer interface {
 	GetCheckCount(context.Context, *GetCheckCountRequest) (*GetCheckCountResponse, error)
+	GetUser(context.Context, *GetUserRequest) (*GetUserResponse, error)
+	UpdateUser(context.Context, *UpdateUserRequest) (*UserTokenResponse, error)
+	ListUsers(context.Context, *ListUsersRequest) (*ListUsersResponse, error)
+	InviteUser(context.Context, *InviteUserRequest) (*InviteUserResponse, error)
+	DeleteUser(context.Context, *DeleteUserRequest) (*DeleteUserResponse, error)
+	GetTeam(context.Context, *GetTeamRequest) (*GetTeamResponse, error)
+	UpdateTeam(context.Context, *UpdateTeamRequest) (*UpdateTeamResponse, error)
+	DeleteTeam(context.Context, *DeleteTeamRequest) (*DeleteTeamResponse, error)
 }
 
 func RegisterCatsServer(s *grpc.Server, srv CatsServer) {
@@ -296,6 +384,150 @@ func _Cats_GetCheckCount_Handler(srv interface{}, ctx context.Context, dec func(
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Cats_GetUser_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetUserRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CatsServer).GetUser(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/opsee.Cats/GetUser",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CatsServer).GetUser(ctx, req.(*GetUserRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Cats_UpdateUser_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UpdateUserRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CatsServer).UpdateUser(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/opsee.Cats/UpdateUser",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CatsServer).UpdateUser(ctx, req.(*UpdateUserRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Cats_ListUsers_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ListUsersRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CatsServer).ListUsers(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/opsee.Cats/ListUsers",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CatsServer).ListUsers(ctx, req.(*ListUsersRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Cats_InviteUser_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(InviteUserRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CatsServer).InviteUser(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/opsee.Cats/InviteUser",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CatsServer).InviteUser(ctx, req.(*InviteUserRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Cats_DeleteUser_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DeleteUserRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CatsServer).DeleteUser(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/opsee.Cats/DeleteUser",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CatsServer).DeleteUser(ctx, req.(*DeleteUserRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Cats_GetTeam_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetTeamRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CatsServer).GetTeam(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/opsee.Cats/GetTeam",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CatsServer).GetTeam(ctx, req.(*GetTeamRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Cats_UpdateTeam_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UpdateTeamRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CatsServer).UpdateTeam(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/opsee.Cats/UpdateTeam",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CatsServer).UpdateTeam(ctx, req.(*UpdateTeamRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Cats_DeleteTeam_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DeleteTeamRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CatsServer).DeleteTeam(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/opsee.Cats/DeleteTeam",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CatsServer).DeleteTeam(ctx, req.(*DeleteTeamRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Cats_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "opsee.Cats",
 	HandlerType: (*CatsServer)(nil),
@@ -303,6 +535,38 @@ var _Cats_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetCheckCount",
 			Handler:    _Cats_GetCheckCount_Handler,
+		},
+		{
+			MethodName: "GetUser",
+			Handler:    _Cats_GetUser_Handler,
+		},
+		{
+			MethodName: "UpdateUser",
+			Handler:    _Cats_UpdateUser_Handler,
+		},
+		{
+			MethodName: "ListUsers",
+			Handler:    _Cats_ListUsers_Handler,
+		},
+		{
+			MethodName: "InviteUser",
+			Handler:    _Cats_InviteUser_Handler,
+		},
+		{
+			MethodName: "DeleteUser",
+			Handler:    _Cats_DeleteUser_Handler,
+		},
+		{
+			MethodName: "GetTeam",
+			Handler:    _Cats_GetTeam_Handler,
+		},
+		{
+			MethodName: "UpdateTeam",
+			Handler:    _Cats_UpdateTeam_Handler,
+		},
+		{
+			MethodName: "DeleteTeam",
+			Handler:    _Cats_DeleteTeam_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
@@ -833,24 +1097,33 @@ var (
 )
 
 var fileDescriptorCats = []byte{
-	// 297 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xe2, 0xe2, 0x4a, 0x4e, 0x2c, 0x29,
-	0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0xcd, 0x2f, 0x28, 0x4e, 0x4d, 0x95, 0x32, 0x48,
-	0xcf, 0x2c, 0xc9, 0x28, 0x4d, 0xd2, 0x4b, 0xce, 0xcf, 0xd5, 0x07, 0x8b, 0xe8, 0x83, 0xa5, 0x93,
-	0x4a, 0xd3, 0x20, 0x5c, 0x30, 0x0f, 0xc2, 0x84, 0x68, 0x94, 0xb2, 0x22, 0x4a, 0x47, 0x49, 0x65,
-	0x41, 0x6a, 0xb1, 0x7e, 0x49, 0x66, 0x6e, 0x6a, 0x71, 0x49, 0x62, 0x6e, 0x01, 0x54, 0xaf, 0x25,
-	0x86, 0xde, 0xa4, 0xc4, 0xe2, 0xcc, 0x64, 0xfd, 0xe2, 0xe4, 0x8c, 0xd4, 0xdc, 0x44, 0xfd, 0xc4,
-	0xf2, 0x62, 0xfd, 0xe4, 0xa2, 0xd4, 0x94, 0xd4, 0xbc, 0x92, 0xcc, 0xc4, 0x9c, 0x62, 0x88, 0x21,
-	0x50, 0xad, 0x1a, 0xf8, 0xb5, 0x96, 0x16, 0xa7, 0x16, 0x41, 0x54, 0x2a, 0x05, 0x73, 0x89, 0xb8,
-	0xa7, 0x96, 0x38, 0x67, 0xa4, 0x26, 0x67, 0x3b, 0xe7, 0x97, 0xe6, 0x95, 0x04, 0xa5, 0x16, 0x96,
-	0x02, 0x5d, 0x21, 0x24, 0xcf, 0xc5, 0x02, 0x52, 0x25, 0xc1, 0xa8, 0xc0, 0xa8, 0xc1, 0x6d, 0xc4,
-	0xad, 0x07, 0xf1, 0x54, 0x28, 0x50, 0x28, 0x08, 0x2c, 0x21, 0x24, 0xc5, 0xc5, 0x01, 0x34, 0xa1,
-	0x28, 0xb1, 0x24, 0x35, 0x45, 0x82, 0x09, 0xa8, 0x88, 0x23, 0x08, 0xce, 0x57, 0xf2, 0xe4, 0x12,
-	0x45, 0x33, 0xb4, 0xb8, 0x20, 0x3f, 0xaf, 0x38, 0x55, 0x48, 0x84, 0x8b, 0x35, 0x19, 0x24, 0x00,
-	0x36, 0x96, 0x29, 0x08, 0xc2, 0xc1, 0x67, 0x94, 0x51, 0x08, 0x17, 0x8b, 0x33, 0x30, 0x1e, 0x84,
-	0x7c, 0xb8, 0x78, 0x51, 0x8c, 0x14, 0x92, 0x86, 0x3a, 0x09, 0x9b, 0xeb, 0xa5, 0x64, 0xb0, 0x4b,
-	0x42, 0x5c, 0xa1, 0xc4, 0xe0, 0xa4, 0xfa, 0xe3, 0xa1, 0x1c, 0xe3, 0x8a, 0x47, 0x72, 0x8c, 0x3b,
-	0x80, 0xf8, 0x04, 0x10, 0x5f, 0x00, 0xe2, 0x07, 0x40, 0x7c, 0x60, 0x91, 0x3c, 0x63, 0x14, 0x3b,
-	0xd0, 0x7f, 0x65, 0x99, 0xc9, 0xa9, 0x49, 0x6c, 0xe0, 0x30, 0x32, 0x06, 0x04, 0x00, 0x00, 0xff,
-	0xff, 0xaf, 0xe0, 0x3e, 0xa6, 0x0b, 0x02, 0x00, 0x00,
+	// 436 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0x8c, 0x93, 0xbf, 0x8e, 0xd3, 0x40,
+	0x10, 0xc6, 0xf1, 0x29, 0xc7, 0x1d, 0x73, 0xa2, 0x59, 0xdd, 0x81, 0xcf, 0xa0, 0x04, 0x45, 0x42,
+	0x4a, 0x15, 0xa3, 0x50, 0x91, 0x0a, 0xc5, 0x48, 0x28, 0x52, 0x2a, 0x93, 0x34, 0x74, 0xeb, 0xcd,
+	0x40, 0xac, 0xc4, 0x7f, 0xf0, 0xae, 0x83, 0x78, 0x1d, 0x2a, 0x1e, 0x81, 0x92, 0x92, 0x92, 0x47,
+	0x00, 0x0a, 0x9e, 0x81, 0x92, 0xf5, 0xae, 0x63, 0xaf, 0x1d, 0x2b, 0xa2, 0x58, 0x29, 0xf3, 0xcd,
+	0x7c, 0x3f, 0x7d, 0x33, 0x91, 0x01, 0x18, 0x15, 0x7c, 0x9c, 0x66, 0x89, 0x48, 0xc8, 0x79, 0x92,
+	0x72, 0x44, 0xe7, 0xd9, 0xfb, 0x50, 0x6c, 0xf2, 0x60, 0xcc, 0x92, 0xc8, 0x55, 0x8a, 0xab, 0xda,
+	0x41, 0xfe, 0x4e, 0x97, 0xaa, 0xd2, 0x3f, 0xb5, 0xd1, 0x99, 0xfe, 0x97, 0x43, 0x7c, 0x4a, 0x91,
+	0xbb, 0x22, 0x8c, 0x90, 0x0b, 0x1a, 0xa5, 0xa5, 0xf7, 0xc5, 0x91, 0x37, 0xa0, 0x3c, 0x64, 0x2e,
+	0x67, 0x1b, 0x8c, 0xa8, 0x4b, 0x3f, 0x72, 0x97, 0x65, 0xb8, 0xc6, 0x58, 0x84, 0x74, 0xc7, 0x35,
+	0xa4, 0xb4, 0x8e, 0x4e, 0x5b, 0x73, 0x8e, 0x59, 0x39, 0x09, 0x7b, 0x9a, 0x96, 0x61, 0x87, 0x6f,
+	0xe0, 0xfa, 0x35, 0x0a, 0x6f, 0x83, 0x6c, 0xeb, 0x25, 0x79, 0x2c, 0x7c, 0xfc, 0x90, 0xcb, 0x44,
+	0x64, 0x00, 0xbd, 0xc2, 0x61, 0x5b, 0x4f, 0xac, 0xd1, 0xd5, 0xe4, 0x6a, 0xac, 0x17, 0x5c, 0x49,
+	0xc9, 0x57, 0x0d, 0xe2, 0xc0, 0xa5, 0x24, 0x64, 0x54, 0xe0, 0xda, 0x3e, 0x93, 0x43, 0x97, 0x7e,
+	0x55, 0x0f, 0xe7, 0x70, 0xd3, 0x82, 0xf2, 0x34, 0x89, 0x39, 0x92, 0x6b, 0x38, 0x67, 0x85, 0xa0,
+	0xb0, 0x67, 0xbe, 0x2e, 0x4e, 0xa1, 0x26, 0x7f, 0x7a, 0xd0, 0xf3, 0xe4, 0x9f, 0x42, 0x16, 0x70,
+	0xbf, 0xc1, 0x24, 0x8f, 0xca, 0x4c, 0x5d, 0xf1, 0x9d, 0xc7, 0xdd, 0x4d, 0x1d, 0x63, 0x78, 0x87,
+	0x4c, 0xe1, 0x42, 0xb6, 0x8a, 0x75, 0xc8, 0x4d, 0x3d, 0xaa, 0xd6, 0x2b, 0x09, 0x0f, 0xda, 0x72,
+	0xe5, 0x9d, 0x01, 0xac, 0xd2, 0xb5, 0x4c, 0xa7, 0xec, 0xf6, 0xe1, 0x34, 0x95, 0x74, 0x20, 0xd8,
+	0xc6, 0xd1, 0x96, 0xc9, 0x16, 0x63, 0x83, 0xf1, 0x12, 0xee, 0x2d, 0x42, 0xae, 0xc8, 0x9c, 0x3c,
+	0x2c, 0x07, 0x2b, 0xa5, 0x4d, 0x30, 0x1a, 0x15, 0xc1, 0x03, 0x98, 0xc7, 0xfb, 0xb0, 0x95, 0xa2,
+	0x96, 0x0e, 0x8c, 0xdb, 0x8e, 0x8e, 0x09, 0x79, 0x85, 0x3b, 0x6c, 0x41, 0x6a, 0xa9, 0x0d, 0x31,
+	0x3b, 0xad, 0x5b, 0x2e, 0x91, 0x46, 0xe6, 0x2d, 0x8b, 0xba, 0xe3, 0x96, 0x5a, 0x36, 0x03, 0xe8,
+	0xc3, 0x29, 0x7b, 0xf3, 0x96, 0x26, 0xe1, 0xb6, 0xa3, 0x73, 0xbc, 0x45, 0x03, 0x52, 0x4b, 0xdd,
+	0x5b, 0x34, 0x21, 0xb3, 0xa7, 0x7f, 0x7f, 0xf5, 0xad, 0x2f, 0xbf, 0xfb, 0xd6, 0x57, 0xf9, 0xbe,
+	0xcb, 0xf7, 0x43, 0xbe, 0x9f, 0xf2, 0x7d, 0xfb, 0x3c, 0xb0, 0xde, 0x5e, 0xc8, 0x95, 0xf7, 0x21,
+	0xc3, 0xe0, 0xae, 0xfa, 0x6c, 0x9e, 0xff, 0x0b, 0x00, 0x00, 0xff, 0xff, 0x2c, 0x2a, 0xd9, 0xe3,
+	0x2a, 0x04, 0x00, 0x00,
 }
